94c1d9b8990c4fb63c1ad168ffb02259403c06ab
Şeymanur Çürükcü <seymanurcurukcu@gmail.com>
2024-02-14 08:53:06 +0300

    tasima eklendi
    tasima eklendi

---
+++ logikeyv2/logikeyv2/Models/TasimaModel.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrate;
+
+namespace logikeyv2.Models
+{
+    public class TasimaModel
+    {
+        public Arac Arac { get; set; }
+        public Surucu Surucu { get; set; }
+        public TasinacakUrun TasinacakUrun { get; set; }
+        public Tasima Tasima { get; set; }
+
+        public UnListesi UnListesi { get; set; }
+        public Cari CariAlici { get; set; }
+        public Cari CariGonderen { get; set; }
+        public Cari CariOdemeYapan { get; set; }
+
+
+        public CariUcretlendirme CariUcretlendirme { get; set; }
+
+        public AracTip AracTip { get; set; }
+
+      
+    }
+}
